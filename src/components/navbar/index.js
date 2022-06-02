4df26e7de11a3993a60f6b0c8a9bd4f2bfa27319
MarcusGANunes <marcusnunes_11@hotmail.com>
2022-06-02 10:37:17 -0300

    navbar fixed

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,25 +1,28 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 import './navbar.modules.css';
 
 export default function NavbarComponent() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand h1 mb-0" href="/">
+        <div class="container-fluid ms-0">
+          <a class="navbar-brand d-inline-block" href="/">
             <img src="DataTrend.png" alt="" width='250'/>
           </a>
           <button
             class="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarSite"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSite"
+            aria-controls="navbarSite"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSite">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link" href="/">
                   <span className="text">Home</span>
