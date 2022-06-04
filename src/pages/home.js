d8c96b8b8518ef3e4207cc57ce0931a3d11433d4
Rafael <rafollo@live.com>
2022-06-04 16:43:43 -0300

    Made background image at Welcome transparent

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,12 @@ import "../styles/home.modules.css";
 
 export default function Home() {
   return (
-    <section className="background">
-      <div className="headline">
-          <h1>Decida de onde comprar sua energia</h1>
+    <section className="welcome">
+      <div className="background">
+        <div className="headline">
+          <h1>Decida de onde comprar <br></br>sua energia</h1>
           <p>Participe do Crowdfunding</p>
+        </div>
       </div>
     </section>
   );
