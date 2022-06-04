53b8b38ddc993d0bc6c8172eba16a0cb67241372
Rafael <rafollo@live.com>
2022-06-04 15:16:36 -0300

    Added background images

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div>
           <NavbarComponent />
           <RouteComponent/>
           <FooterComponent />
-        </div>
       </BrowserRouter>
     </div>
   );
